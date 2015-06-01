86aa2b03c80f6531d374efce38a60160dfb9e3b8
jaketrent <trent.jake@gmail.com>
2015-05-31 19:59:55 -0600

    setup for compiling src -> lib

--- src/bin/play.js
+++ src/bin/play.js
@@ -4,13 +4,13 @@ require('babel/register')({ stage: 0 })
 var argv = require('minimist')(process.argv.slice(2))
 
 function hasArgs(args) {
-  return Object.keys(argv).length > 1
+  return Object.keys(args).length > 1
 }
 
 if (!hasArgs(argv) || argv.help || argv.h) {
-  require('../lib/help')()
+  require('../help')()
   return
 }
 
 if (argv.init || argv.i)
-  require('../lib/init')()
+  require('../init')()
